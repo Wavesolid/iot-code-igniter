59b36e3b9a1086754655f7f74f7f9afb350da1e2
GifinoThoriq <66865274+GifinoThoriq@users.noreply.github.com>
2021-06-18 22:06:51 +0700

    nyobaphp

---
+++ app/Views/LandingUI.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SpecialCorp</title>
+</head>
+<body>
+    <h1>Selamat Datang Admin</h1>
+    <h2>Aselole</h2>
+</body>
+</html>
