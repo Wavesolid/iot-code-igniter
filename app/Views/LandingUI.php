bfdf458d843299d9312957ce0057ddb911528437
GifinoThoriq <66865274+GifinoThoriq@users.noreply.github.com>
2021-06-22 00:21:47 +0700

    bikin sign up form

--- app/Views/LandingUI.php
+++ app/Views/LandingUI.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='stylesheet' type='text/css' href='style.php' />
-
     <link rel="stylesheet" href="/style/landingPage.css">
 
     <title><?= $titlePage?></title>
@@ -16,5 +15,8 @@
     <a href="/SignUp/">
         <h2>Sign Up</h2>
     </a>
+
+    
+
 </body>
 </html>
--- app/Views/SignUp.php
+++ app/Views/SignUp.php
@@ -4,9 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="/style/SignUp.css">
 </head>
 <body>
-    <h1>Aselole</h1>
+    <div class="container">
+        <div class="signup">
+            <div class="kotak-kiri">
+                <div class="logo">
+                    <img src="/imgOno/logo.png" alt="logo">
+                    <h2>Special</h2>
+                    <h2 class="huruf-kedua">Corp</h2>
+                </div>
+                <div class="judul">
+                    <h2>Sign Up</h2>
+                    <h3>Create your account here</h3>
+                </div>
+                <div class="formulir">
+                    <form>
+                        <input type="text" name="email" placeholder="email">
+                        <input type="text" name="username" placeholder="username">
+                        <input type="text" name="password" placeholder="password">
+                    </form>
+                </div>
+            </div>
+            <div class="kotak-kanan">
+                <img src="/imgOno/dunia.svg" alt="dunia">
+            </div>
+        </div>
+    </div>
+    <script src="https://kit.fontawesome.com/891b41940f.js%22%3E"></script>
 </body>
 </html>
